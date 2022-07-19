717b145258a0f22ab0c32194ac21da24d58523f9
Tochukwu <tochukwuodoeme@gmail.com>
2022-07-19 03:02:39 +0100

    update name and username

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,8 +62,8 @@ function BlurImage({image}: {image: Image}) {
           onLoadingComplete={() => setLoading(false)}
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
     </a>
   );
 }
